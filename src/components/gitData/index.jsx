738ca6a9e83e45a9848733e9a9598e7cf12dcb2f
Al Ho <holub.incorp@gmail.com>
2019-07-24 21:03:33 +0300

    Fixed URLs

--- src/components/gitData/index.jsx
+++ src/components/gitData/index.jsx
@@ -40,7 +40,7 @@ class GitData extends Component {
                   {reposData.map(repo => (
                     <li key={repo.id}>
                       <h3>
-                        <a href={repo.url}>{repo.name}</a>
+                        <a href={repo.html_url}>{repo.name}</a>
                       </h3>
                       <p>{repo.description}</p>
                     </li>
